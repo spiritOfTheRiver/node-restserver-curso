ffc0c5b6a7b184fa9ffe256ca8650c67105a4116
Ricardo Loera <ricardo.loera.sanchez@gmail.com>
2020-06-07 13:47:52 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,9 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Rios:BkYfYiHZRVLOypkC@cluster0-uhula.mongodb.net/cafe';
+    urlDB =  process.env.MONGO_URI;
 }
+
+MONGO_URI
 
 process.env.URLDB = urlDB;
